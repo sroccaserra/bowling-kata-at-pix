300ee82ba93eaa67391242a2ccec8308517bd420
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-13 15:34:04 +0200

    r Use a loop implementation instead of slice

--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -1,13 +1,11 @@
 def compute_score(rolls)
   score = rolls.sum
 
-  frame_index = 0
-  rolls.each_slice(2) do
-    | frame_rolls |
-    if frame_rolls.sum == 10
+  10.times do
+    | frame_index |
+    if rolls[frame_index] + rolls[frame_index + 1] == 10
       score += rolls[frame_index + 2]
     end
-    frame_index += 2
   end
 
   score
