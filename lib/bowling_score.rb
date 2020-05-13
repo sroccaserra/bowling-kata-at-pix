ddfab48f2becd1b09d2d4853928afefb3301b413
ARJ <arj@octo.com>
2020-05-13 16:19:56 +0200

    r extract method for strike? and strike_bonus

--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -1,22 +1,31 @@
+
 def compute_score(rolls)
   score = rolls.sum
 
   10.times do
     | frame_index |
-    if is_spare?(rolls, frame_index)
+    if spare?(rolls, frame_index)
       score += spare_bonus(rolls, frame_index)
-    elsif rolls[frame_index*2] == 10
-      score += rolls[frame_index*2 + 1] + rolls[frame_index*2 + 2] 
+    elsif strike?(frame_index, rolls)
+      score += strike_bonus(frame_index, rolls)
     end
   end
 
   score
 end
 
-def is_spare?(rolls, frame_index)
+def spare?(rolls, frame_index)
   rolls[frame_index*2] + rolls[frame_index*2 + 1] == 10
 end
 
 def spare_bonus(rolls, frame_index)
   rolls[frame_index*2+2]
+end
+
+def strike?(frame_index, rolls)
+  rolls[frame_index * 2] == 10
+end
+
+def strike_bonus(frame_index, rolls)
+  rolls[frame_index * 2 + 1] + rolls[frame_index * 2 + 2]
 end
